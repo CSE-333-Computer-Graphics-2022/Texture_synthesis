74695829c2d19245ac09591ea306073341e8b2e5
ishan19309 <55681336+ishan19309@users.noreply.github.com>
2022-12-07 23:33:25 +0530

    Pattern 3
    Continuous curve pattern

---
+++ code_3/build/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "/home/ishan/Documents/Computer Graphics/Assignments/project/codes/code_3/project"
+  "/home/ishan/Documents/Computer Graphics/Assignments/project/codes/code_3/project.pdb"
+  "CMakeFiles/project.dir/depends/imgui/imgui.cpp.o"
+  "CMakeFiles/project.dir/depends/imgui/imgui.cpp.o.d"
+  "CMakeFiles/project.dir/depends/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/project.dir/depends/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/project.dir/depends/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/project.dir/depends/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/project.dir/depends/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/project.dir/depends/imgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/project.dir/depends/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/project.dir/depends/imgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/project.dir/depends/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/project.dir/depends/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/project.dir/src/main.cpp.o"
+  "CMakeFiles/project.dir/src/main.cpp.o.d"
+  "CMakeFiles/project.dir/src/utils.cpp.o"
+  "CMakeFiles/project.dir/src/utils.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
